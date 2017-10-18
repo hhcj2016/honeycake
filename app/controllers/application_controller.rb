a670f16d9b4055a8278fb7404139d41a0df166ad
Cindy <cindyjiang88@163.com>
2017-10-18 20:04:05 +0800

    cannot add same item in cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+
   def admin_required
    if !current_user.admin?
      redirect_to "/", alert: "You are not admin."
@@ -10,15 +11,18 @@ class ApplicationController < ActionController::Base
   helper_method :current_cart
 
   def current_cart
-   @current_cart ||= find_cart
+      @current_cart ||= find_cart
   end
 
   private
 
+
   def find_cart
    cart = Cart.find_by(id: session[:cart_id])
+   #cart = Cart.find_by_id(session[:cart_id])
    if cart.blank?
      cart = Cart.create
+     #session[:cart_id] = cart.id
    end
    session[:cart_id] = cart.id
    return cart
